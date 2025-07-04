6b8ef872fef377a4a76cd4cd6907bc6b0d34edc6
ZeroUltra <929288107@qq.com>
2025-07-04 17:57:05 +0800

    Update ManuallyReloadDomainTool.cs

--- Editor/ManuallyReloadDomainTool.cs
+++ Editor/ManuallyReloadDomainTool.cs
@@ -235,9 +235,9 @@ namespace Plugins.ManuallyReload
         [Tooltip("是否完全手动Reload 为true时,除了手动外,其他操作均不触发Reload")]
         public bool IsFullyManuallyReload = false;
         [Tooltip("是否监听代码行为 默认当创建/删除(cs/asmdef/asmref)时会触发Reload 设置为true时,代码行为变化不再触发Reload")]
-        public bool IsMonitoringCodeBehavior = true;
+        public bool IsMonitoringCodeBehavior = false;
         [Tooltip("是否Editor代码也需手动Reload 当编辑Editor代码才有效")]
-        public bool IsEditorUseManuallyReload;
+        public bool IsEditorUseManuallyReload = false;
 
         //[Header("Other Setting (Optional)")]
         //[Tooltip("显示编译和Reload耗时日志")]
